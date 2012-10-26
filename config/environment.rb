6a9883aa5c68ee25cd4cd3a9ad5fdb6b5bad4e0f
Lyubomyr <fedorniakl@gmail.com>
2012-10-26 18:08:09 +0300

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,15 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 IssuesTracker::Application.initialize!
-
-config.action_mailer.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
-	:address => 'smtp.gmail.com',
-	:port => 587,
-	:domain => 'gmail.com',
-	:authentication => :plain,
-	:user_name => 'prumat3@gmail.com',
-	:password => 'TestingAccount' }
-
-config.action_mailer.raise_delivery_errors = true
